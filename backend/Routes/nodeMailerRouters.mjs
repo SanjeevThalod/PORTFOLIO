e182a649c4f12442da4c541dc4bca1def6d740dd
SanjeevThalod <thalodsanjeev@gmail.com>
2024-09-03 16:42:50 +0530

    updates

--- backend/Routes/nodeMailerRouters.mjs
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import sendEmail from '../Controllers/nodeMailerController.mjs'
-const router = express.Router();
-
-router.post('/send-email',sendEmail);
-
-export default router;
--- backend/server.mjs
+++
@@ -1,8 +0,0 @@
-import app from './app.mjs';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-app.listen(process.env.PORT,()=>{
-    console.log("Server running on port: ",process.env.PORT);
-})
